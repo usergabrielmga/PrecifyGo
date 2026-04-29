5713d2f0b138ae4d9239b34cf545626cf2408385
Gabriel <gabiwwm@gmail.com>
2026-04-29 06:04:32 -0300

    Atualização

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,13 +7,11 @@ const start = async () => {
       host: '0.0.0.0' 
     })
 
-    console.log('🚀 Server rodando')
+    console.log('Server rodando com sucesso!')
   } catch (err) {
     app.log.error(err)
     process.exit(1)
   }
 }
-fastify.get('/', async () => {
-  return { status: 'API online 🚀' }
-})
+
 start()
