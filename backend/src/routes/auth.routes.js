016a7f911b34b99d796c544860e20fef2da93054
Gabriel <gabiwwm@gmail.com>
2026-04-29 07:49:51 -0300

    Ajustando rotas

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 const AuthController = require('../controllers/auth.controller')
+const backend = import.meta.env.VITE_BACKEND_URL;
 
 async function authRoutes(fastify) {
-  fastify.post('/register', AuthController.register)
-  fastify.post('/login', AuthController.login)
-  fastify.post('/google', AuthController.googleLogin)
+  fastify.post(`${backend}/register`, AuthController.register)
+  fastify.post(`${backend}/login`, AuthController.login)
+  fastify.post(`${backend}/google`, AuthController.googleLogin)
 }
 
 module.exports = authRoutes
