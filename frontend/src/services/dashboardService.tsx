de87e3b0d1c45da33dfc98cd30c2fefe7b0ff7ed
Gabriel <gabiwwm@gmail.com>
2026-05-03 14:48:56 -0300

    Ajustando services dashboard

--- frontend/src/services/dashboardService.tsx
+++ frontend/src/services/dashboardService.tsx
@@ -1,7 +1,9 @@
  // Adjust the import path as needed
 
+ const backend = import.meta.env.VITE_BACKEND_URL;
+
 export async function getDashboardData() {
-  const response = await fetch("http://localhost:3000/dashboard") // GET, sem body
+  const response = await fetch(`${backend}/dashboard`) // GET, sem body
 
   if (!response.ok) {
     const errorText = await response.text()
