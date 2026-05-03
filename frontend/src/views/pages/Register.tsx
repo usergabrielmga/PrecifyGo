7f2a1d54e37f67977227be9a3ce8d2555167b084
Gabriel <gabiwwm@gmail.com>
2026-05-03 13:16:38 -0300

    Ajustando login

--- frontend/src/views/pages/Register.tsx
+++ frontend/src/views/pages/Register.tsx
@@ -7,8 +7,10 @@ import { Link } from 'react-router-dom'
 
 export default function Register() {
   
+    
     const { form, setForm, handleRegister } = useRegister()
-   
+    console.log(form)
+
 
     return (
         <div className="flex flex-col items-center w-90 py-8 bg-[#EA2E52] rounded-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
